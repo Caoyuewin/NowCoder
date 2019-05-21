eb4228655cc61a4569dc391548cbc002916476ee
cao <849807784@qq.com>
2019-05-20 21:18:26 -0400

    2019.05.21

---
+++ Day_1/No_2.cpp
@@ -0,0 +1,37 @@
+#if 0
+输入两个字符串，从第一个字符串中删除所有第二个字符串包含的字符后输出
+#endif
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+#if 0
+class Del_char{
+  public:
+    Del_char(char* str1, char* str2){
+      _str1 = str1;
+      _str2 = str2;
+    }
+    void Delete(){
+      char* p1 = _str1;
+      char* p2 = _str2;
+      while(*p2++){
+        if(*p2 == *p1){
+           
+        }
+      }
+    }
+
+  private:
+    char* _str1;
+    char* _str2;
+}; 
+#endif
+
+
+int main(){
+  string s
+}
