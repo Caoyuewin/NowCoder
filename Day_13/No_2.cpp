ef9b9b406960172e2efffdab1686243fb3cb7459
Caoyuewin <849807784@qq.com>
2019-06-04 05:41:21 -0400

    2019.06.04

---
+++ Day_13/No_2.cpp
@@ -0,0 +1,37 @@
+//跳石板
+
+#include <iostream>
+
+using namespace std;
+
+int find_max(int x, int y){
+  int num = 2;
+  int max_divisor = -1;
+  while(num < x){
+    if(y % num == 0 && x % num == 0){
+      if(max_divisor < num)
+        max_divisor = num;
+    }
+    ++num;
+  }
+  return max_divisor;
+}
+
+int main(){
+  int n, m, steps = 0;
+  cin >> n >> m;
+  while(n < m){
+    int maxnum = find_max(n, m);
+    if(maxnum != -1){
+      n += maxnum;
+      ++steps;
+
+    }
+    else
+      steps = -1;
+  }
+  cout << steps << endl;
+  return 0;
+
+}
+
