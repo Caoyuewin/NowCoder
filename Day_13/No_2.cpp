222ac8b1fcf7035153530487e7946aef051aa02e
Caoyuewin <849807784@qq.com>
2019-06-04 06:24:08 -0400

    2019.06.04

--- Day_13/No_2.cpp
+++ Day_13/No_2.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 int find_max(int x, int y){
-  int num = 2;
+  int num = x - 1;
   int max_divisor = -1;
-  while(num < x){
+  while(num < x && num > 1){
     if(y % num == 0 && x % num == 0){
       if(max_divisor < num)
         max_divisor = num;
     }
-    ++num;
+    --num;
   }
+  
   return max_divisor;
 }
 
