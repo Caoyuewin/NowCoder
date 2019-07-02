97274dc6b8263dfe597a88e20292eaaba753be4d
Caoyuewin <849807784@qq.com>
2019-07-02 02:56:00 -0400

    2019.07.02

---
+++ Day_26/快到碗里来.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+long double r_to_c(long double r){
+  long double c = 0;//周长
+  
+}
+
+void Compare_n_r(string n, string r){
+  if()
+}
+ 
+int main() {
+  string n,r;
+  while(cin >> n >> r){
+    if(n.size() >  r.size())
+      cout << "Yes" << endl;
+    else if(n.size() - r.size() > 2)
+      cout << "No" << endl;
+    else
+      Compare_n_r(n,r);
+
+  }
+  return 0;
+}
