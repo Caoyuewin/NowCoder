da6635c382ad939e3e14fa4278d93f20816376f1
Caoyuewin <849807784@qq.com>
2019-08-25 11:49:55 -0400

    删除公共字符串

--- Day_1/No_2.cpp
+++ Day_1/No_2.cpp
@@ -1,5 +1,16 @@
 #if 0
-输入两个字符串，从第一个字符串中删除所有第二个字符串包含的字符后输出
+标题：删除公共字符 | 时间限制：1秒 | 内存限制：32768K
+输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。例如，输入”They are
+students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”
+输入描述：
+每个测试输入包含2个字符串
+输出描述：
+输出删除后的字符串
+示例1:
+输入
+They are students. aeiou
+输出
+Thy r stdnts.
 #endif
 
 #include <iostream>
@@ -7,7 +18,12 @@
 
 using namespace std;
 
-
+int main(){
+    string s1,s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    
+}
 #if 0
 class Del_char{
   public:
@@ -34,15 +50,17 @@ class Del_char{
 //1.遍历查找删除
 //2.贪心算法
 
-int main(){
-  string A("abcd2342343212677");
-  string B("231");
-  auto cur = A.begin();
-  while(cur != A.end()){
-    if(B.find(*cur) != -1){
-      A.erase(cur);
 
-    }
-  }
 
-}
+//int main(){
+//  string A("abcd2342343212677");
+//  string B("231");
+//  auto cur = A.begin();
+//  while(cur != A.end()){
+//    if(B.find(*cur) != -1){
+//      A.erase(cur);
+//
+//    }
+//  }
+//
+//}
